c62efcb0795117420fff23b4a3b4afb8b8079e3d
EC2 Default User <ec2-user@ip-172-31-91-254.ec2.internal>
2017-12-20 01:50:41 +0000

    hi

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ else
 <body>
     <section class="congratulations">
         <h1>Smashingly superior update Section</h1>
-        <p>Yadda yadda yadda.  PHP Version  <?= phpversion() ?></p>
+        <p>Modified in Cloud9.  PHP Version  <?= phpversion() ?></p>
     </section>
 
     <section class="instructions">
